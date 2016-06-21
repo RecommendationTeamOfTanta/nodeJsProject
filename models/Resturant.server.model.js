7e4a21311a6e191e7abdfa904cd1c4af02f5489e
eissa <eissa3101652.me@gmail.com>
2016-06-21 08:19:15 -0700

    added required fields for recommender engine

--- models/Resturant.server.model.js
+++ models/Resturant.server.model.js
@@ -6,34 +6,42 @@ var schema = mongoose.schema;
 //users
 
 var userFeatures = new schema({
-	couple: Boolean,
-	young: Boolean,
-	smoking: Boolean,
-	rangeOfBudget: Number,
-	religion: String,
-	sport: String,
-	dayOfWeek: String,
-	placeOfBirth: date,
-	tasteOfMusic: String,
-	payingMethod: string
+    userId: { type: ObjectId, ref: 'User' },
+    couple: Boolean,
+    young: Boolean,
+    smoking: Boolean,
+    rangeOfBudget: Number,
+    religion: String,
+    sport: String,
+    dayOfWeek: String,
+    placeOfBirth: date,
+    tasteOfMusic: String,
+    payingMethod: string
 });
 
 // create the usersSchema
 var usersSchema = new schema({
-	name: {
-		first: String,
-		middle: String,
-		last: String
-	},
-	adress: String,
-	theEmail: string,
-	password: String,
-	picture: string,
-	birthDate: { type: date },
-	gender: String,
-	mobile: string,
-	role: String,
-	favourites: [{ type: ObjectId, ref: 'Resturant' }]
+    name: {
+        first: String,
+        middle: String,
+        last: String
+    },
+    adress: String,
+    theEmail: string,
+    password: String,
+    picture: string,
+    birthDate: { type: date },
+    gender: String,
+    mobile: string,
+    role: String,
+    favourites: [{ type: ObjectId, ref: 'Resturant' }],
+    features: userFeatures,
+
+    //the recommendation for the user(user-based)
+    recommended_resturants: [{
+        rest_id: { type: ObjectId, ref: 'Resturant' },
+        rankValue: Number
+    }]
 });
 
 //create the user model from usersSchema
@@ -42,70 +50,71 @@ var User = mongoose.model('User', usersSchema);
 
 //resturants 
 var productRateSchema = new schema({
-	userId: { type: ObjectId, ref: 'User' },
-	productVote: number
+    userId: { type: ObjectId, ref: 'User' },
+    productVote: number
 });
 var productSchema = new schema({
-	name: String,
-	description: String,
-	photo: String,
-	details: {
-		price: Number,
-		size: String
-	},
-	rates: [productRateSchema]
+    name: String,
+    description: String,
+    photo: String,
+    details: {
+        price: Number,
+        size: String
+    },
+    rates: [productRateSchema]
 });
 
 var CategorySchema = new schema({
-	name: String,
-	type: String,
-	photo: String,
-	products: [productSchema]
+    name: String,
+    type: String,
+    photo: String,
+    products: [productSchema]
 });
 
 var resturantRateSchema = new schema({
-	userId: { type: ObjectId, ref: 'User' },
-	rest_id: { type: ObjectId, ref: 'Resturant' },
-	resturantVote: number
+    userId: { type: ObjectId, ref: 'User' },
+    rest_id: { type: ObjectId, ref: 'Resturant' },
+    resturantVote: number
 });
 
 var resturantReviewSchema = new schema({
-	userId: { type: ObjectId, ref: 'User' },
-	restId: { type: ObjectId, ref: 'Resturant' },
-	ReviewTxt: String,
-	review_date: { type: Date, default: Date.now }
+    userId: { type: ObjectId, ref: 'User' },
+    restId: { type: ObjectId, ref: 'Resturant' },
+    ReviewTxt: String,
+    review_date: { type: Date, default: Date.now }
 });
 
 
 var resturantFeatures = new schema({
-	famous: Boolean,
-	smokingArea: Boolean,
-	placeForChildren: Boolean,
-	hasGoodView: Boolean,
-	minimumCharge: Boolean,
-	delivery: Boolean,
-	wifi: Boolean,
-	makeSmallParties: Boolean,
-	noiseLevel: string,
-	stuffBehavior: String,
-	acceptCreditCard: Boolean,
-	outDoorSetting: String,
-	hasTv: Boolean,
-	carBarking: Boolean,
-	reservation: Boolean,
-	petsAllowed: Boolean,
-	_24by7: Boolean,
-	priceRate: String,
-	cleanliness: String,
-	foodQuality: String,
-	serviceQuality: String,
-	formalClothes: Boolean,
-	liveMusic: Boolean,
-	bookToRead: Boolean,
-	openPuffet: Boolean,
-	alchols: Boolean,
-	lightiningRomantic: Boolean,
-	publicToilets: Boolean
+    rest_id: { type: ObjectId, ref: 'Resturant' },
+    famous: Boolean,
+    smokingArea: Boolean,
+    placeForChildren: Boolean,
+    hasGoodView: Boolean,
+    minimumCharge: Boolean,
+    delivery: Boolean,
+    wifi: Boolean,
+    makeSmallParties: Boolean,
+    noiseLevel: string,
+    stuffBehavior: String,
+    acceptCreditCard: Boolean,
+    outDoorSetting: String,
+    hasTv: Boolean,
+    carBarking: Boolean,
+    reservation: Boolean,
+    petsAllowed: Boolean,
+    _24by7: Boolean,
+    priceRate: String,
+    cleanliness: String,
+    foodQuality: String,
+    serviceQuality: String,
+    formalClothes: Boolean,
+    liveMusic: Boolean,
+    bookToRead: Boolean,
+    openPuffet: Boolean,
+    alchols: Boolean,
+    lightiningRomantic: Boolean,
+    publicToilets: Boolean
 
 
 
@@ -114,25 +123,31 @@ var resturantFeatures = new schema({
 
 // create the resturantschema
 var resturantSchema = new schema({
-	name: string,
-	description: string,
-	photo: string,
-	location: String,
-	mobile: String,
-	workingHours: {
-		startAt: String,
-		endAt: String
-	},
-	branches: [{
-		name: String,
-		city: String,
-		latitude: Number,
-		longitude: Number
-	}],
-	features: resturantFeatures,
-	categories:[CategorySchema],
-	rates:[resturantRateSchema],
-	reviews:[resturantReviewSchema]
+    name: string,
+    description: string,
+    photo: string,
+    location: String,
+    mobile: String,
+    workingHours: {
+        startAt: String,
+        endAt: String
+    },
+    branches: [{
+        name: String,
+        city: String,
+        latitude: Number,
+        longitude: Number
+    }],
+    features: resturantFeatures,
+    categories: [CategorySchema],
+    rates: [resturantRateSchema],
+    reviews: [resturantReviewSchema],
+
+    //the recommendation for resturants(item-based)
+    similarResturants: [{
+        rest_id: { type: ObjectId, ref: 'Resturant' },
+        rankValue: Number
+    }]
 });
 
 // build the Resturant model from the resturantSchema
@@ -141,6 +156,6 @@ var Resturant = mongoose.model('Resturant', resturantSchema);
 
 //exporting the models
 module.exports = {
-	User: User,
-	Resturant: Resturant
+    User: User,
+    Resturant: Resturant
 };
